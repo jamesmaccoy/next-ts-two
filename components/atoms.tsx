e76c5ccc9d9297f91cf88e32d8c4a3e23e739479
jamesmaccoy <thankyou.digital@gmail.com>
2024-08-29 15:48:30 +0200

    no tailwind

---
+++ components/atoms.tsx
@@ -0,0 +1,9 @@
+export default async function Atoms() {
+    return (
+      <section>
+        <div className='p-8'>
+          <h2 className='font-semibold'>Select a component to see list of Products</h2>
+        </div>
+      </section>
+    )
+  }
